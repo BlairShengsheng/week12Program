fc3474d7b2fc7c9f1a0d45ae9b462688058407e4
shengshenghuo <shengshenghuo3@gmail.com>
2024-09-19 18:55:13 -0700

    Add User input validation on user signup backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -6,8 +6,24 @@ const bcrypt = require('bcryptjs');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const router = express.Router();
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
+
+
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
       const { credential, password } = req.body;
   
@@ -45,9 +61,29 @@ router.post(
   // Log out
 router.delete(
   '/',
+  validateLogin,
   (_req, res) => {
     res.clearCookie('token');
     return res.json({ message: 'success' });
+  }
+);
+
+// Restore session user
+router.get(
+  '/',
+  validateLogin,
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      };
+      return res.json({
+        user: safeUser
+      });
+    } else return res.json({ user: null });
   }
 );
 
